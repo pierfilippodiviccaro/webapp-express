55224e3b6af62fd87c8f010c7aa7a581c142ea3e
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2026-01-22 18:00:23 +0100

    fix: completato parti  dell'esercizio precedente

--- controller/movie_controller.js
+++ controller/movie_controller.js
@@ -3,37 +3,50 @@ import connection from "../database/db.js";
 //index
 function index(req, res, next) {
     const query = "SELECT * FROM `movies`"
+
     connection.query(query, (err, result) => {
         if (err) return next(err)
+        const movies = result.map((movie) => {
+            return {
+                ...movie,
+                image: "http://localhost:6700" + movie.image
+            }
+
+        })
+
+
+
+
         return res.json({
-            results: result
+            results: movies
         })
     })
 }
-function show(req,res,next){
-    const id= req.params.id
-    const query= "SELECT * FROM `movies` WHERE `id` = ? "
-    connection.query(query, [id],(err,results) => {
-        if(err) return next (err)
-        if(results.length=== 0){
+function show(req, res, next) {
+    const id = req.params.id
+    const query = "SELECT * FROM `movies` WHERE `id` = ? "
+    connection.query(query, [id], (err, results) => {
+        if (err) return next(err)
+            
+        if (results.length === 0) {
             res.status(404)
             return res.json({
-                error:"not found",
-                message:"film non trovato",
+                error: "not found",
+                message: "film non trovato",
             })
         }
         const film = results[0]
-        
-        const reviewsQuery= "SELECT * FORM `reviews` WHERE `movie_id`=? "
-        connection.query(reviewsQuery,[id],(err,rewiewsResult)=>{
-            if(err) return next (err)
-                res.json({
+
+        const reviewsQuery = "SELECT * FROM `reviews` WHERE `movie_id`=? "
+        connection.query(reviewsQuery, [id], (err, rewiewsResult) => {
+            if (err) return next(err)
+            res.json({
                 ...film,
-                reviews:rewiewsResult       
-            
-                })
+                reviews: rewiewsResult
+
+            })
         })
     })
 
 }
-export default { index , show}
+export default { index, show }
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,6 @@
 export default function handleError(err, req, res, next) {
-  res.status(500);
+  res.status(500); 
+  console.error(err);
   return res.json({
     error: process.env.ENVIRONMENT === "development" ? err : "INTERNAL ERROR",
     message: "Errore interno del server",
